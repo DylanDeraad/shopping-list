d95079a87b9931f30f3e0843820514a91b13b891
Dylan DeRaad <dyljdr@gmail.com>
2020-06-28 17:44:52 -0600

    add index.html and script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-window.onload
+window.onload = function () {
+    initShoppingList();
+};
+
+function initShoppingList() {
+    let form = document.getElementById("item-form");
+    console.log(form);
+};
